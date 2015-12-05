1d405e7d43835ca902db93f56612277ab94ef87b
grizeldi <grizeldi@gmail.com>
2015-12-05 18:55:29 +0100

    Created GUI project

---
+++ GUI/src/com/grizeldi/pancakeGUI/Main.java
@@ -0,0 +1,17 @@
+package com.grizeldi.pancakeGUI;
+
+public class Main implements Runnable{
+
+    public static void main(String[] args) {
+        new Thread(new Main()).start();
+    }
+
+    public Main(){
+        //TODO if needed
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
