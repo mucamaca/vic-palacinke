540753641480d4baa4086267cf3e8a92f332936b
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2015-12-10 21:03:47 +0100

    Update comm_test.h

--- Arduino_C_things/tests/comm_test.h
+++ Arduino_C_things/tests/comm_test.h
@@ -1,1 +1,14 @@
-//TODO
+/* 
+Header od testnega fila, za testiranje
+Arduino side komunikacije direktno na racunalniku
+*/
+
+#ifndef Arduino_h
+
+void pinMode(uint8_t pin, uint8_t);
+
+void digitalWrite(uint8_t pin, uint8_t value);
+
+int digitalRead(uint8_t pin);
+
+#endif
