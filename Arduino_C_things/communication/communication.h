92912b6048ce4f5ecd356392c0b967316fbd38ed
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2015-12-10 19:53:05 +0100

    Preprocessor stuff

--- Arduino_C_things/communication/communication.h
+++ Arduino_C_things/communication/communication.h
@@ -1,3 +1,6 @@
+#ifndef communication_h //prerprocessor komande da se izognemu veckratnemu importanju in vsemu sranju
+#define communication_h
+
 typedef char read_t;
 typedef char write_t;
 
@@ -6,3 +9,5 @@ void comm_write(write_t value);
 read_t comm_read();
 
 void cat();
+
+#endif
