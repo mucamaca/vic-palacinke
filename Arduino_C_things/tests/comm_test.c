7fbbc9dcb883dbf408ed5ae1c5b3be6d7a752dc4
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2015-12-11 02:10:29 +0100

    Zdej pa sam se stestiram
    Zeehh.

--- Arduino_C_things/tests/comm_test.c
+++ Arduino_C_things/tests/comm_test.c
@@ -5,7 +5,33 @@ naredi dummy Arduino funkcije, ki namesto s pinov
 berejo s stdina in namesto na pine pise na stdout.
 */
 
-#ifndef Arduino_h
+#ifdef Arduino_h
+# error you cannot run this file on arduino
+#endif
 #include "comm_test.h"
+#include <string.h>
+#include <stdio.h>
 
-void
+char t[1024];
+bool x = false;
+void pinMode(char pin, char mode){
+  if(!x){
+    x=true;
+    do{
+      scanf("%d",t[c]);
+      ++c;
+    }while(t[c-1]!=42);
+  }
+}
+  
+int digitalRead(char pin){
+  for(c=0;c<=1024;c++){
+    if(t[c]==pin){
+      return (int) t[c+1];
+    }
+  }
+}
+
+void digitalWrite(char pin,char value){
+  printf("%c %c\n",pin,value);
+}
