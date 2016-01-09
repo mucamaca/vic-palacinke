78b894e279b404a84ddc364977d9d9dbd2415877
grizeldi <grizeldi@gmail.com>
2016-01-09 16:53:27 +0100

    New project.

---
+++ PCtoArduinoTalker/src/com/grizeldi/pcToArduino/Main.java
@@ -0,0 +1,13 @@
+package com.grizeldi.pcToArduino;
+
+import gnu.io.SerialPort;
+
+public class Main {
+    private SerialPort serialPort;
+    private String portName = "COM3";
+    public static final int BAUD_RATE = 9600;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
