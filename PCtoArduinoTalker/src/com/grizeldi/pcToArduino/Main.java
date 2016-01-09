70bdbda3e5c08a0d523d960c5a37d07989306693
grizeldi <grizeldi@gmail.com>
2016-01-09 17:13:28 +0100

    Work backup

--- PCtoArduinoTalker/src/com/grizeldi/pcToArduino/Main.java
+++ PCtoArduinoTalker/src/com/grizeldi/pcToArduino/Main.java
@@ -1,13 +1,75 @@
 package com.grizeldi.pcToArduino;
 
+import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+/**
+ * Class for sending stuff to Arduino.
+ * Written for Windows.
+ * Goes along with the code in arduino testing folder.
+ */
+public class Main implements Runnable, SerialPortEventListener{
     private SerialPort serialPort;
-    private String portName = "COM3";
+    public static final String PORT_NAME = "COM3";
     public static final int BAUD_RATE = 9600;
 
+    private BufferedReader input;
+    private OutputStream output;
+
     public static void main(String[] args) {
-	// write your code here
+        new Main();
+    }
+
+    public Main() {
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+        //Find the correct port.
+        while (portEnum.hasMoreElements()){
+            CommPortIdentifier currentPortId = (CommPortIdentifier) portEnum.nextElement();
+            if (currentPortId.getName().equals(PORT_NAME)){
+                portId = currentPortId;
+            }
+        }
+        //Check if we found port.
+        if (portId == null){
+            System.out.println("Couldn't find COM3 port.");
+            return;
+        }
+        //Open the port and add a listener.
+        try {
+            serialPort = (SerialPort) portId.open(Main.class.getName(), 2000);
+            serialPort.setSerialPortParams(BAUD_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            output = serialPort.getOutputStream();
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e){
+            System.err.println(e.toString());
+            return;
+        }
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent serialPortEvent) {
+
     }
 }
