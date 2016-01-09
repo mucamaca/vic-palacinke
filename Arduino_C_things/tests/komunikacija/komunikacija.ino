2b3fbbe1d1f725d26be1bebd479ca89ca99a5ced
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-01-09 20:57:35 +0100

    this should be betah

--- Arduino_C_things/tests/komunikacija/komunikacija.ino
+++ Arduino_C_things/tests/komunikacija/komunikacija.ino
@@ -1,12 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  byte i = Serial.read();
-  if (i != -1 && i != 255){
-    Serial.println("Got a byte: " + String(i));
-  }
+  char i = Serial.read();
+  if (i != 255) Serial.println("Got a byte: " + i);
 }
