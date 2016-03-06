b2cdb110c0c72d6f0fc720bbce7495e141d20f4f
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-06 23:11:39 +0100

    Oh yes, i am the great pretender

--- Arduino_C_things/Communication.ino
+++ Arduino_C_things/Communication.ino
@@ -1,9 +1,14 @@
+#include <arduino.h>
+
 int initComm(){
   Serial.begin(9600);
+  return check_comm();
+}
+
+int check_comm(){
   Serial.write((byte)42);
   if(Serial.read()!=42)
     return -1;
   else
     return 0;
 }
-
