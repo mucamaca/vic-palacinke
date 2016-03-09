4c1852cceb1c0b3042116ba86d1fbf620a4cb8e2
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-09 04:16:59 +0100

    Update Arduino_C_things.ino

--- Arduino_C_things/Arduino_C_things.ino
+++ Arduino_C_things/Arduino_C_things.ino
@@ -1,14 +1,36 @@
+/*
+ * main file brez main funkcije, ker je arduino IDE retardiran
+ */
+
+#include <logging.h>
+#include <ultrasonic.h>
+#include <constant.h>
+
 void setup() {
+  write("Starting...");
+  delay(200);
+  write("initializing serial...");
   Serial.begin(9600);
   delay(10);
   
-  if (init_comm()) init_error_write(42);
-  while (init_comm); // repeat until stuff happens
   
+  delay(300);
+  write("Checking for RPi serial connection...")
+  while (init_error_write(init_comm())) delay(200);
+  digitalWrite(13, 0);
+  
+  delay(300);
+  write("Checking for ultrasonic sensor...")
+  while (init_error_write(init_sonic())) delay(200); //repeat until stuff happens
+  digitalWrite(13,0);
   //TODO init knjižnic za senzorje pa to
+  write("Done initializing.");
+  write("Cakam na narocilo") ;
 }
 
 void loop() {
-  while (check_comm()) error_write(42); // vsako iteracijo preveri, ce je pi se na zvezi
+  Serial.flush();
+  while (error_write(check_comm())) delay(200); // vsako iteracijo preveri, ce je pi se na zvezi
+  while (error_write(check_sonic())) delay(100);
   // stuff
 }
