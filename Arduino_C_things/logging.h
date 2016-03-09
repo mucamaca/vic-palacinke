66977d2ab20fda767060601202e8deff0e55f906
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-09 04:33:04 +0100

    Update logging.h

--- Arduino_C_things/logging.h
+++ Arduino_C_things/logging.h
@@ -6,6 +6,10 @@ void write(char *message);
 
 void init_write(char *message);
 
-void error_write(int errno);
+void warning_write(char *message);
 
-void init_error_write(int errno);
+void init_warning_write(char *message);
+
+int error_write(int errno);
+
+int init_error_write(int errno);
