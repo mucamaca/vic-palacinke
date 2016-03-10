e8c27c6aeaf7725c0948a65f2cb5c405e11df255
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-10 11:35:27 +0100

    Update ArduinoListener.java

--- Pi_java_stuff/src/edu/vic/pancake/piSide/netwerking/ArduinoListener.java
+++ Pi_java_stuff/src/edu/vic/pancake/piSide/netwerking/ArduinoListener.java
@@ -1,5 +1,13 @@
 package edu.vic.pancake.piSide.netwerking;
 
 public class ArduinoListener{
-  
+    public static ArduinoListener instance;
+    
+    static {
+        instance = new AruinoListener();
+    }
+    
+    private ArduinoListener(){
+        //Thread thread =  TODO
+    }
 }
