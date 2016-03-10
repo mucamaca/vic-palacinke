1983b8f476b2863d1298bf1172752f7176789bd1
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-10 11:23:13 +0100

    Update logging.ino

--- Arduino_C_things/logging.ino
+++ Arduino_C_things/logging.ino
@@ -8,20 +8,12 @@
  * za preprosta sporocila v stilu "Segrevam grelce" ali "Palacinka je nared"
  */
  
-void write(char *message){
-  Serial.write("Info: ");
+void write(char *message, bool init){
+  Serial.write(127);
+  if(init) Serial.write("Init");
+  Serial.write("Info:");
   Serial.write(*message); // nism preprican, a je tle message al *message, mislm da ta drugo.
   Serial.write('\n');
-}
-
-
-/*
- * za sporocila, ki se posljejo v init fazi programa
- */
- 
-void init_write(char *message){
-  Serial.write("Init: ");
-  write(*message);
 }
 
 
@@ -29,21 +21,13 @@ void init_write(char *message){
  * funkcija za opozorila, npr. "Nizko stanje mase" ali "Zmanjkuje ________ namaza"
  */
  
-void warning_write(char *message){
-  Serial.write("Warning: ");
+void warning_write(char *message, bool init){
+  Serial.write(127);
+  if(init) Serial.write("Init:");
+  Serial.write("Warning:");
   Serial.write(*message);
-  Serial.write("!\n");
+  Serial.write('/n');
 }
-
-/*
- * init-time opozorila
- */
-
-void warning_write(char *message){
-  Serial.write("Init: ");
-  Serial.write(*message);
-}
-
 
 /*
  * za errorje sporoci samo stevilko, opis pa doda java na RPi-ju
@@ -51,24 +35,10 @@ void warning_write(char *message){
  
 int error_write(int errno){
   if (errno) {
-    Serial.write("Error ");
     Serial.write(errno/100+'0');
     Serial.write((errno%100)/10+'0');
     Serial.write(errno(%10)+'0');
-    Serial.write(':');
   }
   return errno;
 }
 
-
-/*
- * init-time errorji
- */
- 
-int init_error_write(int errno){
- if (errno) {
-    Serial.write("Init: ");
-    error_write(errno, *message);
- }
- return errno;
-}
