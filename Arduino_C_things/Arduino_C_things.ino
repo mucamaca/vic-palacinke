3e9a88a16ed31bdc8c40e7c0fb4acb8dc9a1cfbf
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-10 17:40:55 +0100

    Update Arduino_C_things.ino

--- Arduino_C_things/Arduino_C_things.ino
+++ Arduino_C_things/Arduino_C_things.ino
@@ -2,6 +2,7 @@
  * main file brez main funkcije, ker je arduino IDE retardiran
  */
 
+#define bananas "bananas"
 
 void setup() {
   while (init_comm()) delay(200); // 
@@ -10,12 +11,12 @@ void setup() {
 void loop() {
   Serial.flush();
   while (error_write(check_comm())) delay(400); // vsako iteracijo preveri, ce je pi se na zvezi
-  while (error_write(check_())) {
+  while (error_write(check_masa())) {
     delay(200);
-    // stuff todo if there exists not enough 
-  } // preveri, ce ma se dost mase
+    // stuff todo if there exists not enough masa
+  } 
   // stuff
-  write("banananas", false);
+  write(banananas, false);
   delay(1000);
   Serial.write(17);
   Serial.write(103);
