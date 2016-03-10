a3fc4f87ef2248ab7ad28594a4aae8e3bcbc4e64
grizeldi <grizeldi@gmail.com>
2016-03-10 12:23:03 +0100

    ArduinoDebugFilter implementiran

--- Pi_java_stuff/src/edu/vic/pancake/piSide/netwerking/ArduinoDebugFilter.java
+++ Pi_java_stuff/src/edu/vic/pancake/piSide/netwerking/ArduinoDebugFilter.java
@@ -1,26 +1,43 @@
 package edu.vic.pancake.piSide.netwerking;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ArduinoDebugFilter extends Thread{
+    public static boolean shutDown = false;
+
     static {
         new ArduinoDebugFilter().run();
     }
 
     @Override
     public void run() {
-        //FIXME add a graceful shutdown
-        while (true){
+        while (!shutDown){
             try {
                 if (ArduinoCommunication.inUnfiltered.available() > 0){
                     byte b = (byte)ArduinoCommunication.inUnfiltered.read();
                     if (b == 127){
-                        //TODO print all next stuff
+                        char c;
+                        boolean exit = false;
+                        StringBuilder builder = new StringBuilder();
+                        InputStreamReader inRead = new InputStreamReader(ArduinoCommunication.inUnfiltered);
+                        while (!exit){
+                            if (ArduinoCommunication.inUnfiltered.available() > 0) {
+                                c = (char) inRead.read();
+                                if (c == '\n')
+                                    exit = true;
+                                else
+                                    builder.append(c);
+                            }
+                        }
+                        System.out.println(builder.toString());
+                        //TODO log to file
                     }else {
                         ArduinoCommunication.in.add(b);
                     }
                 }
             } catch (IOException e) {
+                System.err.println("Debug filter encountered an error: ");
                 e.printStackTrace();
             }
         }
