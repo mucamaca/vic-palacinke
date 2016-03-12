c37660a976ff02c12ffc8f2b49282899b8a5f5b7
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-12 11:31:56 +0100

    Update servo.ino

--- Arduino_C_things/servo.ino
+++ Arduino_C_things/servo.ino
@@ -3,3 +3,22 @@
  * vsemi funkcijami na servomotorjih, ki jih rabmo.
  */
  
+ Servo dosing_servo;
+ 
+char init_servos(){
+  if (NUMBER_OF_SERVOS == 0) {
+    write("No servos initialized, because of NUMBER_OF_SERVOS set to 0 in constant.ino", 1, 1);
+    return 0;
+  }
+    
+  write("__init__ servo za doziranje", 1, 0);
+  dosing_servo.attach(DOSING_SERVO_PIN);
+  if(!dosing_servo.attached()){
+    return 42; //some to-be-defined nonzero code
+  }
+  write("OK", 1, 0);
+  
+  
+  // other servos.
+}
+ 
