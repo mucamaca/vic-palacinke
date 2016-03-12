abe2b675c78916d89f30087a7b57cd152526ce40
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-12 15:53:43 +0100

    Update logging.ino

--- Arduino_C_things/logging.ino
+++ Arduino_C_things/logging.ino
@@ -3,44 +3,36 @@
  */
 
 
-
 /*
  * za preprosta sporocila v stilu "Segrevam grelce" ali "Palacinka je nared"
  */
  
-void write(const char message[127], bool init){
+void write(const char message[127], bool init, bool warning){
   Serial.write(127);
-  if(init) Serial.write("Init: ");
-  Serial.write("Info:");
+  if (init)
+    Serial.write("[Init]");
+  if (warning)
+    Serial.write("[Warning]:")
+  else
+    Serial.write("[Info]:");
   int i;
   for (i = 0; i < strlen(message); i++){
     Serial.write(message[i]);
   }
   Serial.write('\n');
 }
-
-
-/*
- * funkcija za opozorila, npr. "Nizko stanje mase" ali "Zmanjkuje ________ namaza"
- */
  
-void warning_write(char *message, bool init){
-  Serial.write(127);
-  if(init) Serial.write("Init:");
-  Serial.write("Warning:");
-  Serial.write(*message);
-  Serial.write('\n');
-}
-
+ 
 /*
  * za errorje sporoci samo stevilko, opis pa doda java na RPi-ju
  */
  
-int error_write(int errno){
+char error_write(char errno){
   if (errno) {
+    digitalWrite(13, 1);
     Serial.write(errno/100+'0');
     Serial.write((errno%100)/10+'0');
-    //Serial.write(errno(%10)+'0');
+    Serial.write(errno%10+'0');
   }
   return errno;
 }
