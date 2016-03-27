c40923453b1dd8529f6d7a5d37a5be820ca19ee2
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-03-27 20:25:59 +0200

    Update servo.ino

--- Arduino_C_things/servo.ino
+++ Arduino_C_things/servo.ino
@@ -8,10 +8,9 @@ char make_pancake(){
   Servo servo;
   if (!servo.attach(DOSING_SERVO_PIN))
     return 42; // some not-yet-defined errorcode 1- 63
-  char state=(char)servo.read();
-  dosing_servo.write(state + 0/*required value*/);
+  dosing_servo.write(0/*required value*/);
   delay(0/*required value*/); // some computation comes here
-  dosing_servo.write(state);
+  dosing_servo.write(0/*zacetno stanje*/);
   return 0;
 }
  
