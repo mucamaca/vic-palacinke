65e06d3e60ef690164cb105a87adee88096cdea9
mucamaca <bor.groseljsimic@telemach.net>
2016-04-13 17:25:08 +0200

    mjau snake

--- lcd_test/snake.ino
+++ lcd_test/snake.ino
@@ -3,8 +3,11 @@
 #define SCL_PIN 2
 Adafruit_ssd1306syp display(SDA_PIN,SCL_PIN);
 float x=50,y=50;
+int pos=0;
+char arr[10][2];
 double smer = 0;
-#define hitrost 1.5;
+#define hitrost 1.5
+
 void snakeLoop(){
   //Read input
   update();
@@ -12,8 +15,24 @@ void snakeLoop(){
   delay(30);
 }
 
+void draw_snake(char *snk,int len){
+  int i;
+  for(i=pos;i%len<pos;i++){
+    display.drawPixel(snk[i][0],snk[i][1],WHITE);
+  }
+}
+
+void arr_init(char *snk char x, char y, char len){
+  int i;
+  for(i=0;i<len;i++){
+    snk[i][0]=x++;
+    snk[i][1]=y++;
+  }
+}
+
 void snakeInit(){
   display.initialize();
+  arr_init(x,y,10);
 }
 
 void update(){
