aafcc78f6d238df08376131b736fe1d49fa4ce81
mucamaca <bor.groseljsimic@telemach.net>
2016-04-17 00:34:44 +0200

    improved snake formating

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,17 +1,12 @@
 #include <Adafruit_ssd1306syp.h>
 #define SDA_PIN 13
 #define SCL_PIN 12
-Adafruit_ssd1306syp display(SDA_PIN,SCL_PIN);
+Adafruit_ssd1306syp display(SDA_PIN, SCL_PIN);
 
 struct piece{
   char x;
   char y;
 };
-
-long ranx=120,rany=43;
-char obstacle_len=0;
-struct piece *obst;
-  
 
 struct snake_t{
   char x;
@@ -23,56 +18,49 @@ struct snake_t{
   struct piece *arr;
 };
 
+long ranx = 120, rany = 43;
 struct snake_t *snake;
-
-
-
-//Bodi pozorna na to: ce vektorsko kolici in pri krozenju gre prav za to: amplituda ostane sicer ves cas enaka, ampak smer se pa spreminja. in ker je pospesek definiran kot sprememba hitrosti deljena s casom, preprosto ni mogoce spremenit hitrost, ne da bi bil pri tem udelezen pospesek. in se bolj neposredna kot zveza med spremembo hitrosti pa pospeskom je zveza med pospeskom in silo. vmes je samo eno mnozenje oziroma deljenje z maso, ki pa tako ali tako ni vektorska kolicina, torej nima smeri, zato jo skoraj lahko zanemarimo pri tej razlagi.
-//in zdaj si zamisli tisti avto, z zacetka razlage. naenkrat zavije na levo. Kam bo torej kazala sprememba hitrosti, levo ali desno? tukaj se zdi zelo ocitno. Seveda bo obrnjena proti levi.
+char obstacle_len = 0;
+struct piece *obst;
 
 
 void update_snake(){
-  snake->arr[snake->index].x=snake->x;
-  snake->arr[snake->index].y=snake->y;
-  snake->index=(snake->index+1)%snake->len;
+  snake->arr[snake->index].x = snake->x;
+  snake->arr[snake->index].y = snake->y;
+  snake->index = (snake->index + 1) % snake->len;
 }
   
 
-
 void move(){
-  char dx=0,dy=0;
-  if(analogRead(5) - 512<-100){
-    dx=-1;
-    Serial.println("x < 0");
+  char dx = 0,dy = 0;
+  if(analogRead(5) - 512 < -100){
+    dx = -1;
   }
-  else if(analogRead(5) - 512>100){
-    dx=1;
-    Serial.println("x > 0");
+  else if(analogRead(5) - 512 > 100){
+    dx = 1;
   }
-  if(analogRead(4) - 512<-100){
-    dy=1;
-    Serial.println("y < 0");
+  if(analogRead(4) - 512 < -100){
+    dy = 1;
   }
-  else if(analogRead(4) - 512>100){
-    dy=-1;
-    Serial.println("y > 0");
+  else if(analogRead(4) - 512 > 100){
+    dy = -1;
   }
-  bool mov=1;
-  if(dx&&!dy)
-    snake->x+=4*dx;
-  else if(dy&&!dx)
-    snake->y+=4*dy;
-  else if(dy&&dx){
-    snake->y+=2*dy;
-    snake->x+=2*dx;
+  bool mov = 1;
+  if(dx && !dy)
+    snake->x += 4 * dx;
+  else if(dy && !dx)
+    snake->y += 4 * dy;
+  else if(dy && dx){
+    snake->y += 2 * dy;
+    snake->x += 2 * dx;
   }else{
-    snake->x+=2*snake->ldx;
-    snake->y+=2*snake->ldy;
-    mov=0;
+    snake->x += 2 * snake->ldx;
+    snake->y += 2 * snake->ldy;
+    mov = 0;
   }
   if(mov){
-    snake->ldx=dx;
-    snake->ldy=dy;
+    snake->ldx = dx;
+    snake->ldy = dy;
   }
   
   //popravi tole, da se ne bo dalo ful ostro zavijat
@@ -89,18 +77,18 @@ void update(){
 }
 
 void draw_snake(){
-  char i=0;
-  while(i++<snake->len){
-    if((i+1)%snake->len!=snake->index)
-      display.drawLine(snake->arr[i%snake->len].x, snake->arr[i%snake->len].y, 
-          snake->arr[(i+1)%snake->len].x, snake->arr[(i+1)%snake->len].y, WHITE);
+  char i = 0;
+  while(i++ < snake->len){
+    if((i + 1) % snake->len != snake->index)
+      display.drawLine(snake->arr[i % snake->len].x, snake->arr[i % snake->len].y, 
+          snake->arr[(i + 1) % snake->len].x, snake->arr[(i + 1) % snake->len].y, WHITE);
   }
 }
 
 void end_game(){
   char i;
   delay(200);
-  for(i=0;i<5;i++){
+  for(i = 0; i < 5; i++){
     render();
     delay(100);
   }
@@ -112,7 +100,7 @@ void end_game(){
 void render(){
   display.clear();
   draw_snake();
-  display.drawPixel(snake->x, snake->y,WHITE);
+  display.drawPixel(snake->x, snake->y, WHITE);
   display.update();
 }
 
@@ -121,8 +109,8 @@ void setup(){
   snake=(struct snake_t *)malloc(sizeof(struct snake_t));
   //snake_init(snake);
   char i;
-  for(i=1;i<snake->len*4;i+=4){
-    display.drawPixel(snake->x, snake->y+i, WHITE);
+  for(i = 1; i < snake->len * 4; i += 4){
+    display.drawPixel(snake->x, snake->y + i, WHITE);
   }
 }
 
