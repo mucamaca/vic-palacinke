8772769993ba7547e562159b1651e0d05025dea4
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-04-17 20:08:06 +0200

    Update constant.ino

--- Arduino_C_things/constant.ino
+++ Arduino_C_things/constant.ino
@@ -1,6 +1,6 @@
 /*
  * File z definiranimi stevilkami portov in podobnimi constant stvarmi.
- * Notr das konstanto v obliki #define _ime_spremenljivke_ _value_
+ * Notr das konstanto v obliki #define IME_KONSTANTE value
  *
  * Naj samo spomnim, da na pinih 0 in 1 ne sme bit nic zaradi seriala.
  */
@@ -8,9 +8,14 @@
 #define DOSING_SERVO_PIN 10
 
 
-// termometer-stuff {
-#define NUMBER_OF_ONEWIRE_PINS 1
+// grelci-stuff
+#define THERMO_VCC 
+#define THERMO_GND
+#define THERMO_CLK
+#define THERMO_CS
+#define THERMO_DO
 
-#define ONEWIRE_PIN 6
+#define SPODNJI_GRELEC
+#define ZGORNJI_GRELEC
 
 // add other stuff here
