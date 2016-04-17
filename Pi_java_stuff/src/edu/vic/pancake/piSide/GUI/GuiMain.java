2dff443321031c3cbdd899ee3fcdbc1110d198cf
grizeldi <grizeldi@gmail.com>
2016-04-17 11:07:53 +0200

    Added .idea to gitignore

--- Pi_java_stuff/src/edu/vic/pancake/piSide/GUI/GuiMain.java
+++ Pi_java_stuff/src/edu/vic/pancake/piSide/GUI/GuiMain.java
@@ -1,15 +1,34 @@
 package edu.vic.pancake.piSide.GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class GuiMain extends Application{
+public class GUIMain extends Application {
+    public static GUIMain guiMain;
+
     Stage stage;
+    Scene splashScene;
+
+    public GUIMain() {
+        guiMain = this;
+    }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        this.stage = stage;
-        stage.setTitle("Palačinke GUI");
+    public void start(Stage primaryStage) throws Exception{
+        Parent fxml = FXMLLoader.load(getClass().getResource("res/splash_screen.fxml"));
+
+        splashScene = new Scene(fxml, 800, 450);
+
+        stage = primaryStage;
+        stage.setTitle("PfankuchenMacher9000 GUI");
+        stage.setScene(splashScene);
         stage.show();
+    }
+
+    public void switchScreen(){
+        System.out.println("Switch screen called.");
     }
 }
