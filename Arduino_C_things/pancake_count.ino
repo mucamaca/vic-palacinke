40b9352d5aa46b0fc9f5e87306e556e4c00f8e7a
mucamaca <bor.groseljsimic@telemach.net>
2016-05-11 20:37:17 +0200

    meow

--- Arduino_C_things/pancake_count.ino
+++ Arduino_C_things/pancake_count.ino
@@ -1,8 +1,13 @@
-union pancake_count{
+union pancake_counter_t{
   char segments[2];
-  unsigned short whole;
+  uint16_t whole;
 };
 
-
+uint16_t read_pancake_number(){
+  union pancake_counter_t num_of_pancakes;
+  num_of_pancakes.segments[0] = EEPROM[0];
+  num_of_pancakes.segments[1] = EEPROM[1];
+  return num_of_pancakes.whole;
+}
 
 
