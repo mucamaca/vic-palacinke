324b292a752194324f1feeb5c8dd7e25f7a0d697
grizeldi <grizeldi@gmail.com>
2016-06-03 16:59:22 +0200

    Dodana fundacija za nov screen

--- Pi_java_stuff/src/edu/vic/pancake/piSide/GUI/controllers/WipScreenController.java
+++ Pi_java_stuff/src/edu/vic/pancake/piSide/GUI/controllers/WipScreenController.java
@@ -1,4 +1,17 @@
 package edu.vic.pancake.piSide.GUI.controllers;
 
-public class WipScreenController {
+import edu.vic.pancake.piSide.GUI.GuiMain;
+import edu.vic.pancake.piSide.GUI.ScreenListener;
+import edu.vic.pancake.piSide.GUI.Screens;
+
+public class WipScreenController implements ScreenListener{
+
+    public WipScreenController() {
+        GuiMain.controllers.put("Working", this);
+    }
+
+    @Override
+    public void onScreenSwitched(Screens screen) {
+        //TODO
+    }
 }
