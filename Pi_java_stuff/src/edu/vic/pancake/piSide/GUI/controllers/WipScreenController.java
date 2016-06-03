9361270f45481cf5a7f59776101791d91c649c85
grizeldi <grizeldi@gmail.com>
2016-06-03 17:33:21 +0200

    Mal design dela

--- Pi_java_stuff/src/edu/vic/pancake/piSide/GUI/controllers/WipScreenController.java
+++ Pi_java_stuff/src/edu/vic/pancake/piSide/GUI/controllers/WipScreenController.java
@@ -3,8 +3,10 @@ package edu.vic.pancake.piSide.GUI.controllers;
 import edu.vic.pancake.piSide.GUI.GuiMain;
 import edu.vic.pancake.piSide.GUI.ScreenListener;
 import edu.vic.pancake.piSide.GUI.Screens;
+import javafx.scene.control.ProgressBar;
 
 public class WipScreenController implements ScreenListener{
+    public ProgressBar progressBar;
 
     public WipScreenController() {
         GuiMain.controllers.put("Working", this);
