d3e06ef8a1eaadffe184d82aeefade6d4a083aa7
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-06-12 21:10:41 +0200

    Update Arduino_C_things.ino

--- Arduino_C_things/Arduino_C_things.ino
+++ Arduino_C_things/Arduino_C_things.ino
@@ -4,11 +4,6 @@
 
 #include "constant.h"
 
-long all_steps = 0;
-long pancake[2];
-bool should_dispense = true;
-bool is_baking = false; 
-
 // k33pt4lk1ng4ndn013xpl0d3s
 
 void setup(){
@@ -33,28 +28,6 @@ void setup(){
 
 void loop(){
   check_grelci();
-  /*step_trak(&all_steps);
-  int i;
-  for(i = 0; i < 2; i++){
-    /*switch(all_steps - pancake[i]){
-      case RAZDALJA_DO_GRELCEV:
-        write("going to bake...", 0,0);
-        bake();
-        break;
-      case RAZDALJA_DO_ZVIJANJA:
-        write("going to roll", 0,0);
-        zvij();
-        break;
-      case END_OF_TRAK:
-        write("going to dispense",0,0);
-        dispense_pancake();
-        break;
-    }
-  }
-  check_grelci();
-  //write("cycle_completed",0,0);
-  */
-  //dispense_pancake();
   delay(2000);
 }
 
