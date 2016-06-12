30744e8e9d34bc3524d6899b9d97a21cffa845db
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-06-12 21:46:28 +0200

    Update infrared.ino

--- Arduino_C_things/infrared.ino
+++ Arduino_C_things/infrared.ino
@@ -2,9 +2,13 @@
  * Funkcije za branje inputa z infrardečega senzorja in ugotavljanje, a je palačinka, al je ni.
  */
  
-bool read_infrared(){
-  if(analogRead(0) > 500)
-    return 1;
-  else
-    return 0;
+void read_infrared(){
+  if(!pancake_found && analogRead(0) > 500){
+    pancake_found = 1;
+  }else if(pancake_found && analogRead(0) < 500){
+    pancake_found = 0;
+    delay(200);
+    digitalWrite(ATTINY_PIN, 0);
+    baking_start = millis();
+  }
 }
