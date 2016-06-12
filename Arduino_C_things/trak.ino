e17970a14bd64a124b2dd64d87c281d5aa1f9938
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-06-12 20:51:37 +0200

    Update trak.ino

--- Arduino_C_things/trak.ino
+++ Arduino_C_things/trak.ino
@@ -1,24 +1,9 @@
 /**
- * File s funkcijami za nadzor na premikanjem traka.
+ * File s funkcijami za nadzor nad premikanjem traka.
  */
 
-#include <Timer.h>
 
-Timer t;
-
-
-char trak_init(){
-  pinMode(TRAK_PUL_PIN, OUTPUT);
-  t.oscillate(TRAK_PUL_PIN, 1, 1); // podpicje, ostani tukaj z mano
-
-  pinMode(TRAK_DIR_PIN, OUTPUT);
-  digitalWrite(TRAK_PUL_PIN, 1); // ce se vrti v napacno smer nastav to na 0
+void trak_init(){
+  pinMode(ATTINY_PIN, OUTPUT);
 }
 
-void step_trak(long * steps){
-  if(is_baking)
-    return;
-  t.update();
-  (*steps)++; 
-  Serial.println(*steps);
-}
