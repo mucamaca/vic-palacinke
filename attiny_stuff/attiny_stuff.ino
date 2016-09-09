a3d892af05601f2b7ccec385193b73fa48cc783a
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-09-09 16:54:36 +0200

    #DEIFNE

--- attiny_stuff/attiny_stuff.ino
+++ attiny_stuff/attiny_stuff.ino
@@ -3,15 +3,18 @@
  */
 
 #include <Stepper.h>
+#define FROM_ARDUINO_PIN 0
+#define STEPPER_PUL 2
+#define STEPPER_DIR 1
 
-Stepper stepper(400, 1, 0);
+Stepper stepper(400, STEPPER_PUL, STEPPER_DIR);
 
 void setup() {
-  pinMode(2,INPUT);
+  pinMode(FROM_ARDUINO_PIN, INPUT);
 }
 
 void loop() {
-  if(digitalRead(2))
+  if(digitalRead(FROM_ARDUINO_PIN))
     stepper.step(5);
   else
     stepper.step(-5);
