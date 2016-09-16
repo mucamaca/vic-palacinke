92d50bfb2b267422045324526939ff8ca2426cf4
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-09-16 22:04:06 +0200

    stupid comments

--- Arduino_C_things/error.h
+++ Arduino_C_things/error.h
@@ -1,19 +1,13 @@
 #define ULTRASONIC_ERROR 1
-// 1
 
 #define MASA_ERROR 2
-// 2
 
 #define ZGORNJI_THERMOCOUPLE_ERROR 4
-//4
 
 #define SPODNJI_THERMOCOUPLE_ERROR 8
-//8
 
 #define HANDSHAKE_TIMED_OUT 16
-//16
 
 #define WRONG_HANDSHAKE 32
-//32
 
 uint64_t *error_bit_mask;
