f1dc21269e3e6feeb0e0778324926c973ebd1fb4
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2017-01-05 15:05:48 +0100

    sprememba v nacinu voznje

--- Arduino_C_things/trak.ino
+++ Arduino_C_things/trak.ino
@@ -7,10 +7,9 @@
 static Stepper trak_stepper(800, TRAK_PUL_PIN, TRAK_DIR_PIN);
 
 void trak_move(int steps){
-  int i, mili_remainder, integer_part;
-  float float_part, steps_per_second = RAZDALJA_MED_GRELCI / BAKE_TIME; 
-  integer_part = (int) steps_per_second
-  mili_remainder = millis();
-  
-  
+  int i; 
+  for(i=0;i<RAZDALJA_MED_GRELCI;i++){
+    trak_stepper.step(1);
+    delay(16+(i%3 && 1));
+  }  
 }
