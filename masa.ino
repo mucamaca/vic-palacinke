34054ae32edfff5d1c1930427018bab34b6ff10a
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2019-11-19 16:34:48 +0100

    isdfj

--- masa.ino
+++ masa.ino
@@ -1,4 +1,4 @@
-/**
+/*
  * File s funkcijami za doziranje, merjenje in mesanje mase.
  */
 
@@ -8,45 +8,36 @@
 
 Stepper dosing_stepper(80000, DOSING_PUL_PIN, DOSING_DIR_PIN);
 
-void dosing_enable(){
-  digitalWrite(DOSING_ENA_PIN,1);
+void dosing_enable() {
+	digitalWrite(DOSING_ENA_PIN, 1);
 }
 
-void dosing_disable(){
-  digitalWrite(DOSING_ENA_PIN,0);
+void dosing_disable() {
+	digitalWrite(DOSING_ENA_PIN, 0);
 }
 
-void dosing_setup(){
-  pinMode(DOSING_ENA_PIN, OUTPUT);
-  delay(200);
-  
-  dosing_disable();
-  Serial.println("mhaahhahhaha");
-  while(!Serial.available()) delay(1);
-  Serial.read();
-  dosing_enable();
-  Serial.println("meiwooeu");
-  for(int i=0;i<(DOZIRANJE_START+DOZIRANJE_STEP)/2;i++){
-    dosing_stepper.step(-2);
-    delay(1);
-  }
+void dosing_setup() {
+	pinMode(DOSING_ENA_PIN, OUTPUT);
+	delay(200);
+
+	dosing_disable();
 }
 
 int nalij(int steps)
 {
-  
-  int i, mil;
-  delay(100);
-  for(i=0;i<steps/2;i++){
-    dosing_stepper.step(2);
-    delay(1);
-  }
-  delay(8000);
-  for(i=0;i<steps/2;i++){
-    dosing_stepper.step(-2);
-    delay(1);
-  }
-  delay(50);
-
-  return mil;
+	dosing_enable();
+	int i, mil;
+	delay(100);
+	for (i = 0; i < steps / 2; i++) {  //  spusti palco
+		dosing_stepper.step(1);
+		delay(DOZIRANJE_DELAY);
+	}
+	delay(6000);  //  kokcajta se masa naliva na trak
+	for (i = 0; i < steps / 2; i++) {  // dvigne palco
+		dosing_stepper.step(-1);
+		delay(2);
+	}
+	delay(50);
+	dosing_disable();
+	return mil;
 }
--- vic-palacinke.ino
+++ vic-palacinke.ino
@@ -1,48 +1,38 @@
-/**
+/*
  * main file brez main funkcije, ker je arduino DDE (Disintegrated Development Environment) neumen.
  */
- 
+
 #include "constant.h"
 
 char trak_moving = 0;
 
 #define DBG
 
-void setup()
-{
+void setup() {
+	pinMode(STEPPER_5V_PIN, OUTPUT);
+	digitalWrite(STEPPER_5V_PIN, 1);
 
-    
-  pinMode(STEPPER_5V_PIN, OUTPUT);
-  digitalWrite(STEPPER_5V_PIN, 1);
+	Serial.begin(9600);
+	Serial.println("Running...");
 
-  Serial.begin(9600);
-  Serial.println("Running...");  
+	heating_setup();
 
-    heating_setup();
+	trak_setup();
 
-  trak_setup();
-    
-  dosing_setup();
-  pinMode(12,OUTPUT);
-  digitalWrite(12, 1);
+	dosing_setup();
+	pinMode(12, OUTPUT);
+	digitalWrite(12, 1);
 }
-
-
-
 
 int co=1;
-void loop()
-{
-  Serial.println("lkhasdf");
- long m=millis();
- //nalij(DOZIRANJE_STEP);
- //nalij(DOZIRANJE_STEP);
-  while(millis()-m< BAKE_TIME*1000){
-    delay(BAKE_TIME);
-  }
-  
-  trak_move();
-    int i=0;
-    nalij(DOZIRANJE_STEP);
-}
+void loop() {
+	Serial.println("lkhasdf");
+	long m=millis();
+	while(millis() - m< BAKE_TIME*1000) {
+		delay(BAKE_TIME);
+	}
 
+	trak_move();
+	int i = 0;
+	nalij(DOZIRANJE_STEP);
+}
